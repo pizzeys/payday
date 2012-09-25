02ec0ccfd17f08c0ccbd30500b2dc917f39e84b3
Alan Johnson <alan@commondream.net>
2012-09-24 21:51:04 -0700

    Got tests moved over to RSpec.
    One more test is failing, but it should be a quick fix.

---
+++ spec/line_item_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+module Payday
+  describe LineItem do
+    it "should be able to be initilized with a price" do
+      li = LineItem.new(:price => BigDecimal.new("20"))
+      expect(li.price).to eq(BigDecimal.new("20"))
+    end
+
+    it "should be able to be initialized with a quantity" do
+      li = LineItem.new(:quantity => 30)
+      expect(li.quantity).to eq(BigDecimal.new("30"))
+    end
+
+    it "should be able to be initlialized with a description" do
+      li = LineItem.new(:description => "12 Pairs of Pants")
+      expect(li.description).to eq("12 Pairs of Pants")
+    end
+
+    it "should return the correct amount" do
+      li = LineItem.new(:price => 10, :quantity => 12)
+      expect(li.amount).to eq(BigDecimal.new("120"))
+    end
+  end
+end
+
