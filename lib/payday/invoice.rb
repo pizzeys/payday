411654a95604ec4c6162b574cc7a6d41b7981219
David Poetzsch-Heffter <david.poetzsch-heffter@twail.net>
2017-12-20 22:02:22 +0100

    fixed issue #53

--- lib/payday/invoice.rb
+++ lib/payday/invoice.rb
@@ -26,12 +26,12 @@ module Payday
 
     # The tax rate that we're applying, as a BigDecimal
     def tax_rate=(value)
-      @tax_rate = BigDecimal.new(value.to_s)
+      @tax_rate = BigDecimal.new(value.nil? ? 0 : value.to_s)
     end
 
     # Shipping rate
     def shipping_rate=(value)
-      @shipping_rate = BigDecimal.new(value.to_s)
+      @shipping_rate = BigDecimal.new(value.nil? ? 0 : value.to_s)
     end
   end
 end
